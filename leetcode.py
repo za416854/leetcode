5097ef2f9e9c9ef50f83855fcfb83959da81a303
Kris Hu <wenhsuanhu0710@gmail.com>
2025-07-02 16:07:07 +0000

    exponent: Linked List Cycle

--- leetcode.py
+++ leetcode.py
@@ -51,3 +51,34 @@ class Solution:
 
 # 為何要加一? 因為要放index = 0的第一個元素時，它最多只會在 index 0~2 出現（共 k+1 = 3 個位置）
 
+
+class ListNode:
+    def __init__(self, val=0, next=None):
+        self.val = val
+        self.next = next
+
+def has_cycle(self, head: ListNode) -> bool:
+    slow = head
+    fast = head
+    while fast and fast.next:
+        slow = slow.next
+        fast = fast.next.next
+        if fast == slow:
+            return True
+    return False
+
+# debug your code below
+node1 = ListNode(1)
+node2 = ListNode(2)
+node3 = ListNode(3)
+node4 = ListNode(4)
+
+# creates a linked list with a cycle: 1 -> 2 -> 3 -> 4 -> 2 (cycle)
+node1.next = node2
+node2.next = node3
+node3.next = node4
+node4.next = node2 
+
+print(has_cycle(node1))
+
+
