95c3d7517da15481ad9fe4202f2a8f6ae8310c4f
Kris Hu <z416854@hotmail.com>
2025-12-01 23:57:46 +0000

    392. Is Subsequence

--- leetcode_in_Salford.py
+++ leetcode_in_Salford.py
@@ -1020,8 +1020,15 @@ class SmallestInfiniteSet:
                 write += 1
         for i in range(write, len(nums)):
             nums[i] = 0
-            
-                
+    # 392. Is Subsequence    
+    def isSubsequence(self, s: str, t: str) -> bool:
+        s_index = 0
+        t_index = 0
+        while s_index < len(s) and t_index < len(t):
+            if s[s_index] == t[t_index]:
+                s_index += 1
+            t_index += 1
+        return True if s_index == len(s) else False
                 
             
             
