4b587f38fe296431b0b2e93a0ab0ecf3328a097a
Kris Hu <z416854@hotmail.com>
2025-12-02 22:00:40 +0000

    1004. Max Consecutive Ones III

--- leetcode_in_Salford.py
+++ leetcode_in_Salford.py
@@ -1128,6 +1128,22 @@ class SmallestInfiniteSet:
                 curr_counters += 1
             max_counter = max(curr_counters, max_counter)
         return max_counter
+    # 1004. Max Consecutive Ones III
+    def longestOnes(self, nums: List[int], k: int) -> int:
+        start = 0
+        length = len(nums)
+        max_lenth = 0
+        zero_count = 0
+        for end in range(length):
+            if nums[end] == 0:
+                zero_count += 1
+            while zero_count > k:
+                if nums[start] == 0:
+                    zero_count -= 1
+                start += 1
+            curr_length = end - start + 1
+            max_lenth = max(curr_length, max_lenth)
+        return max_lenth
             
                 
             
