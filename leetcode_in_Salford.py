707e4b1a726e57f26523402acb8e6fac42bb95d5
Kris Hu <z416854@hotmail.com>
2025-12-11 00:31:05 +0000

    2215. Find the Difference of Two Arrays

--- leetcode_in_Salford.py
+++ leetcode_in_Salford.py
@@ -1190,7 +1190,20 @@ class SmallestInfiniteSet:
                 return i
             left_sum += nums[i]
         return -1
-                
+    
+    # 2215. Find the Difference of Two Arrays
+    def findDifference(self, nums1: List[int], nums2: List[int]) -> List[List[int]]:
+        set1 = set(nums1)
+        set2 = set(nums2)
+        res1 = []
+        for num in set1:
+            if num not in set2:
+                res1.append(num)
+        res2 = []
+        for num in set2:
+            if num not in set1:
+                res2.append(num)
+        return [res1, res2]
         
                 
             
